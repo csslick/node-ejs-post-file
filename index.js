13df70d6025e3442f205747d3724991d9f218665
JuHwan <juhwan203@kakao.com>
2022-12-21 11:07:57 +0900

    최초 작성 - 폼에서 post 글 쓰기

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const fs = require('fs');
+
+let posts = [];  // 글 데이터
+// 파일 읽기
+const readFile = fs.readFileSync('postDB.json', 'utf-8');
+// 오브젝트 코드로 변환
+const jsonData = JSON.parse(readFile);
+console.log(jsonData)
+posts = [...jsonData]; // post에 배열값 추가
+
+// ejs를 view 엔진으로 설정
+app.set('view engine', 'ejs');
+
+// 정적파일 경로 지정
+app.use(express.static("public"));
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// home
+app.get('/', function(요청, 응답){
+  응답.render('pages/index.ejs', { posts })
+})
+
+// about
+app.get('/about', function(req, res) {
+  res.render('pages/about.ejs')
+})
+
+// 글쓰기 요청 /create
+app.post('/create', function(req, res) {
+  const 글 = req.body.post;
+  posts.push(글); // posts 배열에 글 추가
+  console.log('posts = ', posts);
+  // DB file에 글 저장
+
+})
+
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`server running at ${port}`)
+})
